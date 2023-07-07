21741ac203330396dab6c470e74cfb3268420094
BarysCh <bchernetsov@gmail.com>
2023-07-07 10:21:26 +0300

    task1 corrected, functions added

--- homework#6/task1.py
+++ homework#6/task1.py
@@ -1,10 +1,27 @@
-with open("file1.txt", "w") as f:
-    f.write("12345")
+def write_into_file(file, content):
+    """opens a txt.file, writes content into it"""
+    with open(file, "w") as f:
+        f.write(content)
 
-with open("file1.txt", "r") as f:
-    content = f.readline()
-    if len(content) < 3:
-        print("Error")
-    else:
-        print(content[0], content[1], content[-2], content[-1])
+
+def read_file(file, *args):
+    with open(file, "r") as f:
+        content = f.readline()
+        if len(content) < 3:
+            print("Error")
+        else:
+            for arg in args:
+                print(content[arg])
+
+
+new_file = "file1.txt"
+file_content = "12345"
+
+
+write_into_file(new_file, file_content)
+read_file(new_file, 0, 1, -2, -1)
+
+
+
+
 
