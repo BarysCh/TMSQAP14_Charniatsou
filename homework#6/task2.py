ea1c5744051575865d3b5763b0b13a4050c10dbe
BarysCh <bchernetsov@gmail.com>
2023-07-07 10:36:21 +0300

    task2 corrected, functions added

--- homework#6/task2.py
+++ homework#6/task2.py
@@ -1,20 +1,30 @@
-with open("file2.txt", "w") as f:
-    f.write("123456")
+def write_into_file(file, content):
+    """writes content into a txt. file"""
+    with open(file, "w") as f:
+        f.write(content)
 
-with open("file3.txt", "w") as f:
-    f.write("")
 
-with open("file4.txt", "w") as f:
-    f.write("")
+def distribute_content(file,file_1, file_2):
+    """distributes content of one file into two others"""
+    with open(file, "r") as f:
+        content = f.readline()
+        for i in content:
+            i = int(i)
+            if i % 2 == 0:
+                with open(file_1, "a") as f:
+                    f.write(str(i))
+            else:
+                with open(file_2, "a") as f:
+                    f.write(str(i))
 
-with open("file2.txt", "r") as f:
-    content = f.readline()
-    for i in content:
-        i = int(i)
-        if i % 2 == 0:
-            with open("file3.txt", "a") as file:
-                file.write(str(i))
-        else:
-            with open("file4.txt", "a") as file:
-                file.write(str(i))
 
+new_file = "file2.txt"
+file_content = "123456"
+file1 = "file3.txt"
+file2 = "file4.txt"
+new_file_content = ""
+
+write_into_file(new_file, file_content)
+write_into_file(file1, new_file_content)
+write_into_file(file2, new_file_content)
+distribute_content(new_file, file1, file2)
